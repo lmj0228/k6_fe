6fa7f6c13274660730e6101de9696d269f6317a6
lmj0228 <wise887@naver.com>
2024-02-22 12:53:44 +0900

    자바스크립트 함수

--- 03/03.js
+++ 03/03.js
@@ -1,3 +1,10 @@
-function handleClick() {
-    document.getElementById("msgArea").innerHTML = "안녕하세요" ;
+function handleClick(n) {
+    // 메세지 영역 가져오기1
+    // document.getElementById("msgArea").innerHTML = "안녕하세요" ;
+
+    // 메세지 영역 가져오기2
+    //document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>" ;
+
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다.</h2>` ;
+    // 백틱 ` : 숫자키 1 왼쪽. ${n}
 }
